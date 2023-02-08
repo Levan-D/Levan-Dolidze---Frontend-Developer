638a86d4fe9e157253774ef9ef6318cd01ad1dee
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-02-08 20:02:18 +0400

    a

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -7,6 +7,17 @@ export const store = configureStore({
   reducer: {
     getData: getDateReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore these action types
+        ignoredActions: ["data/Get/fulfilled", "data/Get/rejected"],
+        // Ignore these field paths in all actions
+        ignoredActionPaths: ["meta.arg", "payload.timestamp"],
+        // Ignore these paths in the state
+        ignoredPaths: ["items.dates"],
+      },
+    }),
 })
 
 export type RootState = ReturnType<typeof store.getState>
